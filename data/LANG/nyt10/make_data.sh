5dd7cdbf10296bae92900a4c5b0f18831c7a55a0
Fenia Christopoulou <efstathia.christopoulou@manchester.ac.uk>
2021-06-06 19:40:51 +0100

    move files

--- data/LANG/nyt10/make_data.sh
+++ data/LANG/nyt10/make_data.sh
@@ -6,7 +6,8 @@ wget https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/nyt10/nyt10_tr
 mv nyt10_train.txt nyt10_train_full.txt
 
 # Split Data
-python clean_n_split.py --input_data nyt10_train_full.txt \
-                        --train_data nyt10_train.txt \
-                        --r2id nyt10_rel2id.json \
-                        --val_data nyt10_val.txt
+cd ../
+python clean_n_split.py --input_data nyt10/nyt10_train_full.txt \
+                        --train_data nyt10/nyt10_train.txt \
+                        --r2id nyt10/nyt10_rel2id.json \
+                        --val_data nyt10/nyt10_val.txt
